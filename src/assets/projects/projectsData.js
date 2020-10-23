257d601a19527c850642bd25df1c1849102030c5
Margarida Sousa <margaridasousa@MBPdeMargarida2.lan>
2020-10-23 21:31:43 +0100

    filter buttons css and filtering done, data from file

---
+++ src/assets/projects/projectsData.js
@@ -0,0 +1,178 @@
+import charactersProject from '../../assets/images/CharactersList.png';
+import CocktailAlchimia from '../../assets/images/CocktailAlchimia.png';
+import AGP from '../../assets/images/AGP.png';
+import Porto from '../../assets/images/Porto.png';
+import myWeather from '../../assets/images/myWeather.png';
+import Windbnb from '../../assets/images/Windbnb.png';
+import RecipePage from '../../assets/images/RecipePage.png';
+import TakeALook from '../../assets/images/TakeaLook.png';
+
+export const projectsData = [
+  {
+    id: 0,
+    title: "Associação Guias de Portugal",
+    description: "Implementation of the front and back-end of the client’s new website based on a mockup. A Backoffice was also designed and developed to meet the client need to update the website.",
+    image: AGP,
+    date: "May ~ October 2020",
+    keywords: "HTML, CSS, Bootstrap, JavaScript, React, Responsive, Backoffice, NodeJS, Nodemailer, Express, mySQL, Github, Git, VSCode, GroupProject, Freelancer, Remote, FullStack",
+    link: "https://agp-projeto-heroku.herokuapp.com/"
+  },
+  {
+    id: 1,
+    title: "Discover Porto!",
+    description: "Responsive website development about the city of Porto. The user can explore what the city has to offer from restaurants, hotels, monuments, event, among other informations.",
+    image: Porto,
+    date: "February 2020",
+    keywords: "HTML, CSS, Bootstrap, JavaScript, Responsive, Github Pages, Github, Git, VSCode, GroupProject, Remote, FrontEnd, Wild Code School",
+    link: "https://margsousa.github.io/wcsPorto/"
+  },
+  {
+    id: 2,
+    title: "Cocktail Alchimia",
+    description: "Dynamic webapp where the user can search by ingredient or drink name and it is offered a list of recipes that can then be shared with friends by email or social media.",
+    image: CocktailAlchimia,
+    date: "March 2020",
+    keywords: "HTML, CSS, Bootstrap, JavaScript, React, API, Responsive, Netlify, Github, Git, VSCode, GroupProject, Remote, FrontEnd, Wild Code School",
+    link: "https://cocktailalchimia.netlify.app/"
+  },
+  {
+    id: 3,
+    title: "Medication Reminder",
+    description: "Dynamic webapp where the user can search by ingredient or drink name and it is offered a list of recipes that can then be shared by email or social media.",
+    image: charactersProject,
+    date: "May 2020",
+    keywords: "HTML, CSS, JavaScript, React, API, Responsive, Github, Git, VSCode, GroupProject, Hackathon, Remote, FullStack, Wild Code School",
+    link: ""
+  },
+  {
+    id: 4,
+    title: "Take a Look",
+    description: "Hackathon “Travel from your couch” app development in 36h using an API. The user can virtually travel around the world by choosing one of 101 webcams available from 101 countries.",
+    image: TakeALook,
+    date: "April 2020",
+    keywords: "HTML, CSS, Bootstrap, JavaScript, React, API, Github Pages, Responsive, Github, Git, VSCode, GroupProject, Hackathon, Remote, FrontEnd, Wild Code School",
+    link: "https://antoniobranco-coder.github.io/Hackaton1/"
+  },
+  {
+    id: 5,
+    title: "myWeather",
+    description: "Weather react app development. The user is given a weekly forecast of their current location but it can also search the weather of all over the world.",
+    image: myWeather,
+    date: "June 2019",
+    keywords: "HTML, CSS, Bootstrap, JavaScript, React, Responsive, Netlify, Github, Git, VSCode, FrontEnd, SheCodesPlus",
+    link: "https://margsousa-myweather-reactapp.netlify.app/"
+  },
+]
+
+export const practiceProjectsData = [
+  {
+    id: 0,
+    title: "Windbnb",
+    description: "DevChallenge to create a responsive small version of Airbnb using a mockup and given data. The user can see all available properties in Finland, filter by city and number of guests.",
+    image: Windbnb,
+    date: "October 2020",
+    keywords: "HTML, CSS, JavaScript, React, Responsive, Github, Git, VSCode, Netlify, Practice, DevChallenges, FrontEnd",
+    link: "https://gifted-fermat-2e3857.netlify.app/"
+  },
+  {
+    id: 1,
+    title: "Recipe Page",
+    description: "DevChallenge to create a responsive recipe page following the given mockup. The user can see the recipe instructions, ingredients, times and can check if it has all the ingredients.",
+    image: RecipePage,
+    date: "October 2020",
+    keywords: "HTML, CSS, JavaScript, React, Responsive, Github, Git, VSCode, Netlify, Practice, DevChallenges, FrontEnd",
+    link: "https://frosty-villani-8a8b7f.netlify.app/"
+  },
+  {
+    id: 2,
+    title: "Characters List",
+    description: "Practice challenge to develop a responsive list using React and Redux where the user can create, add and delete characters from the list.",
+    image: charactersProject,
+    date: "October 2020",
+    keywords: "HTML, CSS, Bootstrap, Modal, JavaScript, React, Redux, Responsive, Github, Git, VSCode, Practice, FrontEnd",
+    link: "https://margsousa-redux-characters-list.netlify.app"
+  },
+]
+
+export const filterOptionsData = [
+  {
+    id: 0,
+    title: "HTML",
+    status: "inactive"
+  },
+  {
+    id: 1,
+    title: "CSS",
+    status: "inactive"
+  },
+  {
+    id: 2,
+    title: "Bootstrap",
+    status: "inactive"
+  },
+  {
+    id: 3,
+    title: "JavaScript",
+    status: "inactive"
+  },
+  {
+    id: 4,
+    title: "React",
+    status: "inactive"
+  },
+  {
+    id: 5,
+    title: "Hooks",
+    status: "inactive"
+  },
+  {
+    id: 6,
+    title: "Context",
+    status: "inactive"
+  },
+  {
+    id: 7,
+    title: "Redux",
+    status: "inactive"
+  },
+  {
+    id: 8,
+    title: "RestAPI",
+    status: "inactive"
+  },
+  {
+    id: 9,
+    title: "NodeJS",
+    status: "inactive"
+  },
+  {
+    id: 10,
+    title: "mySQL",
+    status: "inactive"
+  },
+  {
+    id: 11,
+    title: "GroupProject",
+    status: "inactive"
+  },
+  {
+    id: 12,
+    title: "Hackathon",
+    status: "inactive"
+  },
+  {
+    id: 13,
+    title: "Freelancer",
+    status: "inactive"
+  },
+  {
+    id: 14,
+    title: "FrontEnd",
+    status: "inactive"
+  },
+  {
+    id: 15,
+    title: "FullStack",
+    status: "inactive"
+  },
+];
--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -1,104 +1,58 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { projectsData, practiceProjectsData, filterOptionsData } from '../../assets/projects/projectsData';
 import Header from '../Header/Header';
-import charactersProject from '../../assets/images/CharactersList.png';
-import CocktailAlchimia from '../../assets/images/CocktailAlchimia.png';
-import AGP from '../../assets/images/AGP.png';
-import Porto from '../../assets/images/Porto.png';
-import myWeather from '../../assets/images/myWeather.png';
-import Windbnb from '../../assets/images/Windbnb.png';
-import RecipePage from '../../assets/images/RecipePage.png';
-import TakeALook from '../../assets/images/TakeaLook.png';
 import './Portfolio.css';
 
 const Portfolio = () => {
-
-  const projects = [
-    {
-      id: 0,
-      title: "Associação Guias de Portugal",
-      description: "Implementation of the front-end and back-end of the client’s new website. Also a Backoffice was design and developed by us to meet the client’s needs to update the website content.",
-      image: AGP,
-      date: "May ~ October 2020",
-      keywords: "HTML, CSS, Bootstrap, JavaScript, React, Responsive, Backoffice, NodeJs, Nodemailer, Express, mySQL, Github, Git, VSCode, Group Project, Freelance, Remote, Full-stack",
-      link: "https://agp-projeto-heroku.herokuapp.com/"
-    },
-    {
-      id: 1,
-      title: "Discover Porto!",
-      description: "Responsive website development about the city of Porto. The user can explore what the city has to offer from restaurants, hotels, monuments, event, among other informations.",
-      image: Porto,
-      date: "February 2020",
-      keywords: "HTML, CSS, Bootstrap, JavaScript, Responsive, Github Pages, Github, Git, VSCode, Group Project, Remote, Front-end, Wild Code School",
-      link: "https://margsousa.github.io/wcsPorto/"
-    },
-    {
-      id: 2,
-      title: "Cocktail Alchimia",
-      description: "Dynamic webapp where the user can search by ingredient or drink name and it is offered a list of recipes that can then be shared with friends by email or social media.",
-      image: CocktailAlchimia,
-      date: "March 2020",
-      keywords: "HTML, CSS, Bootstrap, JavaScript, React, API, Responsive, Netlify, Github, Git, VSCode, Group Project, Remote, Front-end, Wild Code School",
-      link: "https://cocktailalchimia.netlify.app/"
-    },
-    {
-      id: 3,
-      title: "Medication Reminder",
-      description: "Dynamic webapp where the user can search by ingredient or drink name and it is offered a list of recipes that can then be shared by email or social media.",
-      image: charactersProject,
-      date: "May 2020",
-      keywords: "HTML, CSS, JavaScript, React, API, Responsive, Github, Git, VSCode, Group Project, Hackathon, Remote, Full-stack, Wild Code School",
-      link: ""
-    },
-    {
-      id: 4,
-      title: "Take a Look",
-      description: "Hackathon “Travel from your couch” app development in 36h using an API. The user can virtually travel around the world by choosing one of 101 webcams available from 101 countries.",
-      image: TakeALook,
-      date: "April 2020",
-      keywords: "HTML, CSS, Bootstrap, JavaScript, React, API, Github Pages, Responsive, Github, Git, VSCode, Group Project, Hackathon, Remote, Front-end, Wild Code School",
-      link: "https://antoniobranco-coder.github.io/Hackaton1/"
-    },
-    {
-      id: 5,
-      title: "myWeather",
-      description: "Weather react app development. The user is given a weekly forecast of their current location but it can also search the weather of all over the world.",
-      image: myWeather,
-      date: "June 2019",
-      keywords: "HTML, CSS, Bootstrap, JavaScript, React, Responsive, Netlify, Github, Git, VSCode, Front-end, SheCodesPlus",
-      link: "https://margsousa-myweather-reactapp.netlify.app/"
-    },
-    {
-      id: 6,
-      title: "Windbnb",
-      description: "DevChallenge to create a responsive small version of Airbnb using a mockup and given data. The user can see all available properties in Finland, filter by city and number of guests.",
-      image: Windbnb,
-      date: "October 2020",
-      keywords: "HTML, CSS, JavaScript, React, Responsive, Github, Git, VSCode, Netlify, Practice, DevChallenges, Front-end",
-      link: "https://gifted-fermat-2e3857.netlify.app/"
-    },
-    {
-      id: 7,
-      title: "Recipe Page",
-      description: "DevChallenge to create a responsive recipe page following the given mockup. The user can see the recipe instructions, ingredients, times and can check if it has all the ingredients.",
-      image: RecipePage,
-      date: "October 2020",
-      keywords: "HTML, CSS, JavaScript, React, Responsive, Github, Git, VSCode, Netlify, Practice, DevChallenges, Front-end",
-      link: "https://frosty-villani-8a8b7f.netlify.app/"
-    },
-    {
-      id: 8,
-      title: "Characters List",
-      description: "Practice challenge to develop a responsive list using React and Redux where the user can create, add and delete characters from the list.",
-      image: charactersProject,
-      date: "October 2020",
-      keywords: "HTML, CSS, Bootstrap, Modal, JavaScript, React, Redux, Responsive, Github, Git, VSCode, Practice, Front-end",
-      link: "https://margsousa-redux-characters-list.netlify.app"
-    },
-  ]
-
-  const filters = ["HTML", "CSS", "Bootstrap", "JavaScript", "React", "Redux", "Responsive", "Project", "Client",
-  "Remote", "Git", "Github", "Netlify", "Github Pages", "NodeJs", "Express", "Freelance", "VSCode", "Front-End", "Full-stack"]
   
+  const [nameFilters] = useState(filterOptionsData);
+  const [ativeFilters, setAtiveFilters] = useState([]);
+  const [projects, setProjects] = useState(projectsData);
+  const [practiceProjects, setPracticeProjects] = useState(practiceProjectsData);
+
+  const handleFilter = (event) => {
+    const { id, name } = event.target;
+    let allOptions = [...nameFilters];
+    let activeOptions = [...ativeFilters];
+    let projectsOptions = [...projects];
+    let practiceOptions = [...practiceProjects];
+    const optionSelected = activeOptions.includes(id);
+
+    if (optionSelected) {
+      activeOptions = activeOptions.filter(value => value !== id);
+      allOptions.map((option) => {
+        if(String(option.id) === id) { 
+          option.status = "inactive"
+        }
+      })
+      if (activeOptions.length === 0) {
+
+      }
+    } else {
+      activeOptions.push(id);
+      allOptions.map((option) => {
+        if(String(option.id) === id) { 
+          option.status = "active"
+        }
+      })
+      projectsOptions = projectsOptions.filter(project =>
+        project.keywords.includes(name) === true
+      )
+      practiceOptions = practiceOptions.filter(project =>
+        project.keywords.includes(name) === true
+      )
+    }
+
+    if (activeOptions.length === 0) {
+      projectsOptions = projectsData;
+      practiceOptions = practiceProjectsData;
+    }
+
+    setProjects(projectsOptions);
+    setPracticeProjects(practiceOptions);
+    setAtiveFilters(activeOptions);
+  }
+
 
   return (
     <div className="Portfolio">
@@ -106,14 +60,16 @@ const Portfolio = () => {
       <div className="portfolio-section">
         <div className="portfolio-main">
           <div className="portfolio-title">Projects</div>
-          <div className="portfolio-subtitle">Filter by:</div>
           <div className="portfolio-filters">
-            {filters.map( button => 
-              <button>{button}</button>
+            <span className="portfolio-subtitle">Filter by:</span>
+            {nameFilters.map((button) => 
+              <button key={button.id} name={button.title} id={button.id} onClick={handleFilter} className={button.status}>
+                {button.title}
+              </button>
             )}
           </div>
           <div className="portfolio-list">
-            {projects.map((project) => 
+            {projects.length > 0 ? projects.map((project) => 
               <div className="project-card" key={project.id}>
                 <div className="project-title">{project.title}</div>
                 <div className="project-date">{project.date}</div>
@@ -123,12 +79,30 @@ const Portfolio = () => {
                 </div>
                 <div className="project-keywords">Keywords: {project.keywords}</div>
               </div>
-            )}
+            )
+            : (<div>Coming soon...</div>)
+            }
+          </div>
+          <div className="portfolio-title">Other Practice Projects</div>
+          <div className="portfolio-list">
+            {practiceProjects.length > 0 ? practiceProjects.map((project) => 
+              <div className="project-card" key={project.id}>
+                <div className="project-title">{project.title}</div>
+                <div className="project-date">{project.date}</div>
+                <div className="project-description">{project.description}</div>
+                <div className="project-image">
+                  <img src={project.image} alt={project.title} />
+                </div>
+                <div className="project-keywords">Keywords: {project.keywords}</div>
+              </div>
+            )
+            : (<div>Coming soon...</div>)
+            }
           </div>
         </div>
       </div>
     </div>
-  );
+  )
 }
  
 export default Portfolio;
